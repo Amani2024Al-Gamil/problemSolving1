50bd058055aced8388e88141bd32f76107330f30
Eng-Amany Al-Gamil Al-tmamy <frontendwebdeveloper186@gmail.com>
2024-12-06 10:29:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ function describeCountry( country, population , capitalCity){
+    return(
+        `${country} has  ${population}  million people and its capital city is  ${capitalCity}`
+    );
+
+   
+
+}
+
+var country1 = describeCountry(`egypt`, 6 , `qahera`);
+var country2 = describeCountry(`baGHDAD` , 5 , `qahera`);
+var country3 = describeCountry(`emarat` , 7 , `dubi`);
+
+console.log(country1);
+console.log(country2);
+console.log(country3);   
